f50c910f31a8a9b21e7daf753d17545641f6c821
Anurag Ghadge <anurag29.ghadge@gmail.com>
2024-10-14 15:34:53 -0500

    Initial Commit

---
+++ FetchListApp/FetchListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchListAppApp.swift
+//  FetchListApp
+//
+//  Created by Anurag Ghadge on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
